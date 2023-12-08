385cb0f1933168721156194ca7d56a3bd590b3de
Filipe Lima <131200594+FilipeLiima@users.noreply.github.com>
2023-12-08 08:02:13 -0300

    upgrade streamlit

--- data.py
+++ data.py
@@ -12,12 +12,58 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
 
 
+# Dataset: Fraudes em Cartões de Crédito
+
+# O conjunto de dados utilizado nesta análise refere-se a transações efetuadas por cartões de crédito em setembro de 2013 por titulares de cartões europeus.
+
+# Overview do Dataset:
+
+# - Total de transações: 284.807
+# - Período: Dois dias
+# - Número de fraudes: 492
+# - Classe positiva (fraudes): 0,172% do total de transações
+
+# Características do Dataset:
+
+# - Variáveis numéricas de entrada
+# - Resultado de uma transformação PCA (Principal Component Analysis)
+# - 14 atributos principais selecionados para este projeto
+# - Atributos originais não fornecidos devido a questões de confidencialidade
+
+# Atributos Principais:
+
+# 1. 'V1' a 'V28': Principais componentes obtidos com PCA
+# 2. 'Tempo': Segundos decorridos entre cada transação e a primeira transação
+# 3. 'Quantidade': Valor da transação
+# 4. 'Classe': Variável de resposta - 1 em caso de fraude, 0 caso contrário
+
+# Desequilíbrio de Classe:
+
+# Devido ao desequilíbrio, a classe positiva (fraudes) representa 0,172% de todas as transações. Recomenda-se medir a acurácia usando a Área sob a Curva de Precisão-Recordação (AUPRC) devido ao desbalanceamento.
+
+#Recomendação:
+
+# Dada a natureza sensível das informações, os atributos originais foram anonimizados para garantir privacidade.
+
+# Esse conjunto de dados é amplamente utilizado em pesquisas sobre detecção de fraudes e fornece uma base sólida para a construção de modelos preditivos eficazes.
+
+
 # Carregamento dos dados
 # URL do arquivo no Google Drive
 google_drive_url = "https://drive.google.com/uc?id=1t3JCQTfZa2qMVb0eRHPOH5d5D5XW_YIk"
 
 # Carregue o arquivo CSV usando o Pandas
 df = pd.read_csv(google_drive_url)
+
+# Análise Descritiva
+df.head(5)
+df.describe()
+df.columns
+
+# Porcentagem de fraudes e não fraudes
+print('Transações não fraudulentas: ', round(df['Class'].value_counts()[0] / len(df) * 100, 2), '% dos dados')
+print('Transações fraudulentas: ', round(df['Class'].value_counts()[1] / len(df) * 100, 2), '% dos dados')
+
 
 # Converte a coluna 'Time' para o formato de data
 df["Time"] = pd.to_datetime(df["Time"])
@@ -30,6 +76,21 @@ y = df["Class"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
 # Criação do modelo preditivo
+
+# Modelo Preditivo: Random Forest Classifier
+
+# O modelo escolhido para esta análise é o Random Forest Classifier. Este é um algoritmo de aprendizado de máquina baseado em ensemble que opera construindo uma "floresta" de árvores de decisão durante o treinamento. Cada árvore na floresta contribui com uma previsão e a classe final é determinada pela votação majoritária das árvores.
+
+# Razões para Escolha do Random Forest:
+
+# 1. Versatilidade e Robustez: O Random Forest é conhecido por ser robusto em várias situações e geralmente não requer muita sintonia de hiperparâmetros. Ele lida bem com conjuntos de dados desequilibrados e tem boa resistência ao overfitting.
+
+# 2. Lida com Dados Não Lineares: Diferentemente de modelos lineares, o Random Forest é capaz de capturar relações não lineares entre as variáveis, tornando-se uma escolha sólida para conjuntos de dados complexos.
+
+# 3. Tratamento Automático de Variáveis Importantes: O modelo fornece naturalmente uma estimativa da importância de cada variável na predição, permitindo uma análise mais profunda sobre quais características têm maior impacto.
+
+# 4. Desempenho Geral: Random Forests tendem a fornecer bom desempenho "out-of-the-box", tornando-se uma escolha eficaz para tarefas de classificação como detecção de fraudes.
+
 modelo_treinado = RandomForestClassifier()
 modelo_treinado.fit(X_train, y_train)
 
--- pages/02_app.py
+++ pages/02_app.py
@@ -3,7 +3,8 @@ import pandas as pd
 import openai
 import datetime
 import joblib
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Carrega o dataframe
 df = st.session_state["data"]
@@ -11,51 +12,57 @@ df = st.session_state["data"]
 # Converte a coluna 'Time' para o formato de data
 df["Time"] = pd.to_datetime(df["Time"])
 
-st.title("Utilize os filtros de análise para gerar interações gráficas:")
-st.markdown(
-    "Recurso interativo em uma aplicação ou plataforma que permite aos usuários analisar e visualizar a incidência de casos de fraude ao longo dos meses do ano. Por meio desse filtro, os usuários podem ajustar a exibição dos dados para focar em um mês específico, possibilitando uma análise mais detalhada da distribuição temporal das fraudes."
-)
+# Campo de entrada para a filtragem no sidebar
+with st.sidebar:
+    fraude_mes = st.slider("Filtragem de ocorrência de fraude por mês:", 1, 12, 1)
+    df_filtered = df[(df["Time"].dt.month == fraude_mes) & (df["Class"] == 1)]
+    st.write(
+        f"Ocorreram {len(df_filtered)} fraudes em {datetime.date(1970, fraude_mes, 1).strftime('%B')}"
+    )
 
-# Filtro slider
-fraude_mes = st.sidebar.slider("Filtragem de ocorrência de fraude por mês:", 1, 12, 1)
-df_filtered = df[(df["Time"].dt.month == fraude_mes) & (df["Class"] == 1)]
-st.sidebar.write(
-    "Ocorreram",
-    len(df_filtered),
-    "fraudes em",
-    datetime.date(1970, fraude_mes, 1).strftime("%B"),
-)
+# Primeira linha com uma coluna
+col1 = st.columns(1)[0]
 
-st.sidebar.markdown("---")
 
-# Campo de entrada para valores das variáveis V1 até V28
-input_values = st.sidebar.text_input(
-    "Modelo Preditivo integrado:",
-    placeholder="Informe os dados",
-)
+# Adiciona espaçamento entre os gráficos
+
+st.markdown("<br>", unsafe_allow_html=True)
 
 # Geração do gráfico do slider fora do bloco condicional
 fraud_months_count = df_filtered["Time"].dt.month.value_counts().sort_index()
 chart_data = pd.DataFrame(
     {
         "month": range(1, 13),
-        "fraudulentas": fraud_months_count.reindex(range(1, 13), fill_value=0),
-        "não fraudulentas": df_filtered[df_filtered["Class"] == 0]["Time"]
+        "fraude": fraud_months_count.reindex(range(1, 13), fill_value=0),
+        "não fraude": df_filtered[df_filtered["Class"] == 0]["Time"]
         .dt.month.value_counts()
         .sort_index()
         .reindex(range(1, 13), fill_value=0),
     }
 )
+# Adiciona um título ao gráfico
+col1.title("Contagem de Ocorrências de Fraude e Não Fraude ao Longo do Ano")
 
 # Widget de gráfico dinâmico fora do bloco condicional
-st.area_chart(chart_data.set_index("month"))
+col1.area_chart(chart_data.set_index("month"))
 
+# Segunda linha
+col1, col2, col3 = st.columns([1, 3, 1])
+
+# Título para o campo de entrada
+col2.title("Modelo Preditivo integrado")
+
+# Campo de entrada para valores das variáveis V1 até V28
+input_values = col2.text_input(
+    "Informe os dados seguindo o padrão:",
+    placeholder="",
+)
 
 # Carrega o modelo treinado
 modelo_treinado = joblib.load("modelo_treinado.joblib")
 
-# Botão para executar a análise
-if st.sidebar.button("Executar Análise") and input_values:
+# Botão para executar a análise ao lado do campo de entrada
+if col2.button("Executar Análise") and input_values:
     # Converta a entrada do usuário para o formato esperado (lista de float, por exemplo)
     novo_dado = [float(valor) for valor in input_values.split(",")]
 
@@ -63,45 +70,36 @@ if st.sidebar.button("Executar Análise") and input_values:
     resultado_predicao = modelo_treinado.predict([novo_dado])
 
     # Organize a exibição dos resultados na barra lateral
-    st.sidebar.write("\n\n\n")
-    st.sidebar.write("Resultado da Análise:")
+    col2.write("\n\n\n")
+    col2.write("Resultado da Análise:")
     if resultado_predicao[0] == 1:
-        st.sidebar.write("Os dados são considerados uma fraude.")
+        col2.write("Os dados são considerados uma fraude.")
     else:
-        st.sidebar.write("Os dados não são considerados uma fraude.")
-
-
-st.sidebar.markdown("---")
-
-
+        col2.write("Os dados não são considerados uma fraude.")
 # IA Generativa
+st.sidebar.title("💬 Chatbot")
+openai_api_key = st.sidebar.text_input(
+    "IA Generativa - OpenAI API Key", key="chatbot_api_key", type="password"
+)
 
-with st.sidebar:
-    openai_api_key = st.text_input(
-        "IA Generativa - OpenAI API Key", key="chatbot_api_key", type="password"
-    )
-
-
-st.title("💬 Chatbot")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        {"role": "assistant", "content": "Como posso te ajudar?"}
-    ]
+    st.session_state["messages"] = [{"role": "assistant", "content": "Como posso te ajudar?"}]
 
 for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+    st.sidebar.write(msg["content"])
 
-if prompt := st.chat_input():
+if prompt := st.sidebar.text_input("Você:", key="user_input"):
     if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
+        st.sidebar.info("Por favor, adicione sua chave da API da OpenAI para continuar.")
         st.stop()
 
     openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=st.session_state.messages
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt='\n'.join(msg["content"] for msg in st.session_state.messages),
+        max_tokens=100,
     )
-    msg = response.choices[0].message
+    msg = {"role": "assistant", "content": response.choices[0].text.strip()}
     st.session_state.messages.append(msg)
-    st.chat_message("assistant").write(msg.content)
+    st.sidebar.write(msg["content"])
